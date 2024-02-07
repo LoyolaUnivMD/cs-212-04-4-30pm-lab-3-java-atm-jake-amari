2b04f8331b33d1e9895fd3446ae1f0d76359cdc4
jakehoogstrate <jakehoogstrate5@gmail.com>
2024-02-07 17:18:12 -0500

    main code

--- Lab3.java
+++ Lab3.java
@@ -12,8 +12,35 @@ import java.util.Scanner;
 
 class Lab3 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
        System.out.println("Welcome to Lab 3");
+        System.out.println("What is your name?");
+        String name = scan.nextLine();
+        double accountBalance = 212.90;
+        int leave = 0;
+        while (leave==0) {
+            System.out.println("Welcome " + name + " Would you like to deposit, withdraw, check balance, or leave?");
+            String choice = scan.nextLine();
+            if (choice.equals("deposit")){
+                System.out.println("How much would you like to deposit?");
+                int deposit = scan.nextInt();
+                accountBalance += deposit;
 
+            }
+            else if (choice.equals("withdraw")){
+                System.out.println("How much would you like to withdraw?");
+                int withdraw = scan.nextInt();
+                accountBalance -= withdraw;
+
+        }
+            else if (choice.equals("check balance")){
+                System.out.println(accountBalance);
 
     }
-}
+            else if (choice.equals("leave")){
+                leave = 1;
+            }
+            else{
+                System.out.print("Invalid input, try again.");
+            }
+}}}
