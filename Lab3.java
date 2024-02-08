07947add2632cc0adf65202ea723633fe4ceecc0
ajthegreat63 <ajpiper@loyola.edu>
2024-02-08 15:41:55 -0500

    Finished algorithm.txt, updated comments and spacing for Lab3.java

--- Lab3.java
+++ Lab3.java
@@ -1,46 +1,52 @@
-// Programmers:  [your names here]
-// Course:  CS 212
-// Due Date:
-// Lab Assignment:
-// Problem Statement:
-// Data In:
-// Data Out:
-// Credits: [Is your code based on an example in the book, in class, or something else?
-//            Reminder: you should never take code from the Internet or another person
+// Programmers: Jake, Amari
+// Course: CS 212
+// Due Date: 2/11/2024
+// Lab Assignment: 3
+// Problem Statement: Develop a program that acts at a smart ATM.
+// Data In: Name, user's choice, withdraw/deposit amount, receipt choice
+// Data Out: Net change in account balance, current account balance
+// Credits: N/A
 
-import java.util.Scanner;
+import java.util.Scanner; // Import Scanner utility
 
 class Lab3 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-       System.out.println("Welcome to Lab 3");
+    public static void main(String[] args) { // Create main method
+        Scanner scan = new Scanner(System.in); // Create Scanner object
+
+        // Prompt user for name, save as string name
+        System.out.println("Welcome to Lab 3");
         System.out.println("What is your name?");
         String name = scan.nextLine();
-        double accountBalance = 212.90;
-        int leave = 0;
-        while (leave==0) {
+
+        double accountBalance = 212.90; // Initialize double accountBalance as 212.90
+        int leave = 0; // Initialize int leave as 0
+
+        while (leave==0) { // While user doesn't choose "leave"
+            // Welcome user using their name and ask what they would like to do, save as string choice
             System.out.println("Welcome " + name + " Would you like to deposit, withdraw, check balance, or leave?");
             String choice = scan.nextLine();
-            if (choice.equals("deposit")){
+
+            if (choice.equals("deposit")){ // If choice is 1 (deposit)
+                // Ask user how much to deposit, save as double deposit
                 System.out.println("How much would you like to deposit?");
                 int deposit = scan.nextInt();
-                accountBalance += deposit;
+                accountBalance += deposit; // Add deposit to accountBalance, replace accountBalance value with result
 
-            }
-            else if (choice.equals("withdraw")){
+            }else if (choice.equals("withdraw")){ // Otherwise if choice is 2 (withdraw)
+                // Ask user how much to withdraw, save as double withdraw
                 System.out.println("How much would you like to withdraw?");
-                int withdraw = scan.nextInt();
-                accountBalance -= withdraw;
+                double withdraw = scan.nextInt();
+                accountBalance -= withdraw; // Subtract withdraw from accountBalance, replace accountBalance value with result
 
-        }
-            else if (choice.equals("check balance")){
-                System.out.println(accountBalance);
+            }else if (choice.equals("check balance")){ // Otherwise if choice is 3 (check balance)
+                System.out.println(accountBalance); // Output current accountBalance
 
-    }
-            else if (choice.equals("leave")){
-                leave = 1;
+            }else if (choice.equals("leave")){ // Otherwise if choice is 4 (leave)
+                leave = 1; // int leave becomes 1
+
+            }else{ // In all other cases (error check)
+                System.out.print("Invalid input, try again."); // Tell user their input is invalid and to try again, replace choice with user input
             }
-            else{
-                System.out.print("Invalid input, try again.");
-            }
-}}}
+        } // End of while loop
+    } // End of main method
+} // End of Lab 3 class
